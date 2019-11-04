24cd2f587e110e7289c7b8bbd2bcfbff6642f4cb
Iván E. Hernández T <iamieht@gmail.com>
2019-11-04 21:15:42 +0100

    Update searchTreeLargeFast.py
    Fixed

--- Week1/searchTreeLargeFast.py
+++ Week1/searchTreeLargeFast.py
@@ -53,16 +53,46 @@ def maxVal(toConsider, avail):
     
     return result
 
-def testMaxVal(foods, maxUnits, printItems = True):
-    print("Use search tree to allocate", maxUnits, 'calories')
-    val, taken = maxVal(foods, maxUnits)
-    print("Total value of item taken =", val)
+def fastMaxVal(toConsider, avail, memo = {}):
+    '''Assumes toConsider a list of subjects, avail a weight
+    memo supplied by Recursive calls
+    Returns a tuple of the total value of a solution to the
+    0/1 knapsack problem and the subjects of that solution'''
+    if (len(toConsider), avail) in memo:
+        result = memo[(len(toConsider), avail)]
+    elif toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0].getCost() > avail:
+        # Explore right branch only
+        result = fastMaxVal(toConsider[1:], avail, memo)
+    else:
+        nextItem = toConsider[0]
+        # Explore left Branch
+        withVal, withToTake = fastMaxVal(toConsider[1:], avail - nextItem.getCost(), memo)
+        withVal += nextItem.getValue()
+        # Explore right Branch
+        withoutVal, withoutToTake = fastMaxVal(toConsider[1:], avail, memo)
+        # Choose better branch
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+    
+    memo[(len(toConsider), avail)] = result
+    return result
 
+
+def testMaxVal(foods, maxUnits, algorithm, printItems = True):
+    print("Menu contains", len(foods), "items")
+    print("Use search tree to allocate", maxUnits, 'calories')
+    val, taken = algorithm(foods, maxUnits)
+    
     if printItems:
+        print("Total value of item taken =", val)
         for item in taken:
             print('  ', item)
 
 
 for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45):
     items = buildLargeMenu(numItems, 90, 250)
-    testMaxVal(items, 750, False)
+    testMaxVal(items, 750, fastMaxVal, True)
