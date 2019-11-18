a029408a6249d2adddde3b13c6ace5ef5d8002d0
Iván E. Hernández T <iamieht@gmail.com>
2019-11-18 15:22:02 +0100

    Update simple_plot.py
    Add Labels and Title

--- Unit2/simple_plot.py
+++ Unit2/simple_plot.py
@@ -14,13 +14,38 @@ for i in range(0, 30):
     myExponential.append(1.5**i)
 
 
-# Plot mySamples (X values) and myLinear (Y values)
+# Plot mySamples (X values) and myXXXX (Y values)
+# Linear Plot
 plt.figure('lin')
+plt.clf()                       # Clear any previously open Figure
+plt.title('Linear') 
+plt.xlabel('sample points')   
+plt.ylabel('linear function')
 plt.plot(mySamples, myLinear)
+
+# Quadratic Plot
 plt.figure('quad')
+plt.clf()                       # Clear any previously open Figure
+plt.title('Quadratic')
+plt.xlabel('sample points')   
+plt.ylabel('quadratic function')
 plt.plot(mySamples, myQuadratic)
+
+# Cubic Plot
 plt.figure('cube')
+plt.clf()                       # Clear any previously open Figure
+plt.title('Cubic')
+plt.xlabel('sample points')   
+plt.ylabel('cubic function')
 plt.plot(mySamples, myCubic)
+
+# Exponencial Plot
 plt.figure('expo')
+plt.clf()                       # Clear any previously open Figure
+plt.title('Exponential')
+plt.xlabel('sample points')   
+plt.ylabel('exponential function')
 plt.plot(mySamples, myExponential)
+
+# Display the plots
 plt.show()
