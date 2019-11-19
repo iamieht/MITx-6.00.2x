866ac76fe84dfc99802b30440c1f65d7fcf15025
Iván E. Hernández T <iamieht@gmail.com>
2019-11-19 08:29:59 +0100

    Create squareRoot.py
    UNIT2: Lecture 5 - squareRoot

---
+++ Unit2/squareRoot.py
@@ -0,0 +1,5 @@
+def squareRoot(x, epsilon):
+    """Assumes x and epsilon are of type float
+                x >= 0 and epsilon > 0
+    Returns float y such that
+            x-epsilon <= y*y <= x+epsilon"""
