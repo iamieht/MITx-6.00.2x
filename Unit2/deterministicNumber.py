63b3c3bb4f83501f5d6dde6d7315114dad71b489
Iván E. Hernández T <iamieht@gmail.com>
2019-11-19 08:44:29 +0100

    Create deterministicNumber.py
    UNIT2: Lecture 5: deterministic Number

---
+++ Unit2/deterministicNumber.py
@@ -0,0 +1,9 @@
+import random
+def deterministicNumber():
+    '''
+    Deterministically generates and returns an even number between 9 and 21
+    '''
+    return 12
+
+
+print(deterministicNumber())
