f9813d298ba040764755d0b57183126fe3f4b14c
Iván E. Hernández T <iamieht@gmail.com>
2019-12-06 19:40:10 +0100

    Create exercise3.py
    Lecture 7: Exercise 3

---
+++ Unit3/Lecture7_Inferential-Statistics/exercise3.py
@@ -0,0 +1,26 @@
+def stdDevOfLengths(L):
+    """
+    L: a list of strings
+
+    returns: float, the standard deviation of the lengths of the strings,
+      or NaN if L is empty.
+    """
+    if (len(L) == 0):
+        return float('NaN')
+    
+    # compute mean first
+    sumVals = 0
+    for s in L:
+        sumVals += len(s)
+    meanVals = sumVals / len(L)
+
+    # compute variance (average squared deviation from mean)
+    sumDevSquared = 0
+    for s in L:
+        sumDevSquared += (len(s) - meanVals)**2
+    variance = sumDevSquared / len(L)
+
+    # standard deviation is the square root of the variance
+    stdDev = variance**(.5)
+
+    return stdDev
